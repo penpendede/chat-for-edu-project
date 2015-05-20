f99ec12209412b1246eebbfadb251309b8921d8f
Simon Seyock <simonseyock@gmail.com>
2015-05-20 23:55:48 +0200

    MessengerController, ConversationController
    MessengerController uses DummyData on any login
    Does not work properly because delegates of Model are missing

---
+++ Chat/Chat/Controller/ConversationController.cs
@@ -0,0 +1,62 @@
+﻿using Chat.Model;
+using Chat.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Chat.Controller
+{
+    public class ConversationController
+    {
+        private UserLocal userLocal;
+        private Conversation conversation;
+        public ConversationTabPage TabPage;
+
+        public ConversationController(UserLocal userLocal, Conversation conversation)
+        {
+            this.userLocal = userLocal;
+            this.conversation = conversation;
+
+            // TODO: bind addMessage to model
+            // TODO: bind conversationOnUserAdd to model
+            // TODO: bind conversationOnUserRemove to model
+
+            TabPage = new ConversationTabPage();
+
+            foreach (User user in conversation.Users)
+            {
+                conversationOnUserAdd(user);
+            }
+
+            foreach (Model.Message message in conversation.Messages)
+            {
+                addMessage(message); 
+                // NOTE: It seems to be right to shorten most delegate names like this if they are doing generic tasks like this
+                // conversation.OnMessageAdd += this.addMessage; // seems quite understandable for me, too
+            }
+        }
+
+        private void addMessage(Model.Message message)
+        {
+            TabPage.AddMessage(message.Sender.Name, message.Text, message.Time);
+        }
+
+        private void conversationOnUserAdd(User user)
+        {
+            TabPage.AddUser(user.Name);
+        }
+
+        private void conversationOnUserRemove(User user)
+        {
+            TabPage.RemoveUser(user.Name);
+        }
+
+        private void tabPageOnTextSubmit(string messageText)
+        {
+            // NOTE: Where does the id come to the model? Maybe the momment it is written into the database? Or should we ask the databaseController (repository) to create a new object?
+            conversation.AddMessage(new Model.Message() { Text = messageText, Sender = this.userLocal, Time = DateTime.Now });
+        }
+    }
+}
---
+++ Chat/Chat/Controller/MessengerController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Chat.Model;
+using Chat.View;
+using System.Windows.Forms;
+
+namespace Chat.Controller
+{
+    public class MessengerController
+    {
+        private UserLocal userLocal;
+        private LoginForm loginForm;
+        private MessengerMainWindowForm mainWindow;
+
+        //private List<ConversationController> conversationControllers; // NOTE: needed
+        //private ConversationTabControl TabControl;
+
+        public MessengerController()
+        {
+            mainWindow = new MessengerMainWindowForm();
+            mainWindow.Hide();
+            mainWindow.FormClosing += this.mainWindowOnClosing;
+
+            loginForm = new LoginForm();
+            loginForm.OnLoginSubmit += this.LoginFormOnSubmit;
+            loginForm.ShowDialog();
+
+            Application.Run(mainWindow);
+        }
+
+        private void LoginFormOnSubmit(string userName, string password)
+        {
+
+            // verify user
+            #warning Not loading user from database
+
+            userLocal = DummyData.CreateUserLocalTest1();
+
+            // TODO: bind delegate OnUserConversationAdd to UserLocal
+
+            mainWindow = new MessengerMainWindowForm();
+
+            foreach (User buddy in userLocal.Buddies)
+            {
+                mainWindow.BuddyListGroupBox.AddBuddy(buddy.Id, buddy.Name);
+            }
+
+            foreach (Conversation conversation in userLocal.Conversations)
+            {
+                userOnConversationAdd(conversation);
+            }
+
+            loginForm.Close(); // NOTE: dispose
+            mainWindow.Show();
+        }
+
+        private void userOnConversationAdd(Conversation conversation)
+        {
+            ConversationController convController = new ConversationController(userLocal, conversation);
+            //conversationControllers.Add(new ConversationController(conversation)) // NOTE:
+            mainWindow.ConversationTabControl.AddTab(convController.TabPage);
+        }
+
+        private void mainWindowOnClosing(object o, EventArgs e)
+        {
+            // TODO: implement
+        }
+    }
+}
